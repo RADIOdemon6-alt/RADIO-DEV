e116cc0bd5f57d5338f6ea9e91fba3ffe8b60586
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-08-17 01:09:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
+// Intro Animation
 window.addEventListener("load", () => {
-  const intro = document.getElementById("intro");
-  const body = document.body;
-
-  // إظهار الانترو
-  intro.style.display = "flex";
-  intro.style.opacity = "1";
-
-  // مدة الانترو
-  setTimeout(() => {
-    intro.style.transition = "opacity 0.5s ease";
-    intro.style.opacity = "0";
-
-    setTimeout(() => {
-      intro.style.display = "none";
-      body.style.visibility = "visible"; // إظهار الصفحة بعد الانترو
-    }, 500);
-  }, 2500); // 2.5 ثانية للتشويش
+setTimeout(() => {
+document.getElementById("intro").style.display = "none";
+}, 2500); // مدة 2.5 ثانية للتشويش
 });
