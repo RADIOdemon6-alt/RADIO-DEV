7adad8939faca12205ba15335a08f26b37710cd6
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-08-16 20:58:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Intro Animation
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("intro").style.display = "none";
+  }, 2500); // مدة 2.5 ثانية للتشويش
+});
