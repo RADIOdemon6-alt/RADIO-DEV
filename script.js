3e6b356c2927f89352678f8d9957f3a7e8167059
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-08-16 21:12:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ window.addEventListener("load", () => {
     document.getElementById("intro").style.display = "none";
   }, 2500); // مدة 2.5 ثانية للتشويش
 });
+document.addEventListener("mousemove", e => {
+  document.body.style.setProperty('--x', e.clientX + "px");
+  document.body.style.setProperty('--y', e.clientY + "px");
+});
